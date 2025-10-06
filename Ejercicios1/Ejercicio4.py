697d1fea1c9c4c4267ef45f9fb90e4372cb25135
Marcos Bolívar <mbolmun688@ieszaidinvergeles.org>
2025-10-06 12:16:06 +0200

    Ejercicio1.4

---
+++ Ejercicios1/Ejercicio4.py
@@ -0,0 +1,6 @@
+#Escribir un programa que muestre por pantalla el resultado de la siguiente operación aritmética.
+multi = 2 * 5
+suma = 2 + 3
+division = suma / multi
+total = division * division
+print(total)
