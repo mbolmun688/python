091c330fa6e5e1306bfdacb864b22286977327ef
Marcos Bolívar <mbolmun688@ieszaidinvergeles.org>
2025-10-16 09:38:48 +0200

    Ejercicio1.11

--- Ejercicios1/Ejercicio10.py
+++ Ejercicios1/Ejercicio10.py
@@ -4,4 +4,9 @@
 #cada paquete a demanda. Cada payaso pesa 112 g y cada muñeca 75 g. Escribir un
 #programa que lea el número de payasos y muñecas vendidos en el último pedido y
 #calcule el peso total del paquete que será enviado.
-payasos = 
+payasos = (int(input("Cuantos payasos quieres: ")))
+muñecas = (int(input("Cuantas muñecas quieres: ")))
+paya = 112
+muñe = 75
+pesoTotal = ((paya * payasos)+(muñe * muñecas))
+print("El peso total es de:",pesoTotal,"g")
