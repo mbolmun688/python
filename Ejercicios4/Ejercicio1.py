ea276045ac437296a95d79b4a8e262aedbad0de7
Marcos Bolívar <mbolmun688@ieszaidinvergeles.org>
2025-10-23 11:13:42 +0200

    Ejercicio4.1

---
+++ Ejercicios4/Ejercicio1.py
@@ -0,0 +1,7 @@
+#Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10
+#veces.
+palabra = str(input("Ingrese una palabra: "))
+contador = 0
+while contador < 10:
+    print(palabra)
+    contador = contador + 1
