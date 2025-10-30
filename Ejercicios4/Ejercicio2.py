226c2fca99925e15ae304857203628e9302b7ee5
Marcos Bolívar <mbolmun688@ieszaidinvergeles.org>
2025-10-30 09:47:07 +0100

    Ejercicio4.2

---
+++ Ejercicios4/Ejercicio2.py
@@ -0,0 +1,7 @@
+#Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos
+#los años que ha cumplido (desde 1 hasta su edad).
+anos = int(input("Inserte los años que tienes: "))
+contador = 0
+while contador <= anos :
+    print(contador)
+    contador = contador + 1
