6445238eacc81ae678edcb9844d50ad923486228
Marcos Bolívar <mbolmun688@ieszaidinvergeles.org>
2025-10-30 11:11:15 +0100

    Ejercicio4.7

---
+++ Ejercicios4/Ejercicio7.py
@@ -0,0 +1,6 @@
+#Escribir un programa que muestre por pantalla la tabla de multiplicar del 1 al 10.
+multi = 10
+num = 1
+while num <= 10:
+    print(num, "* " "10 = ", (num * multi))
+    num = num + 1
