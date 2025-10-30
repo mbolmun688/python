9a8c615cb7f536f3f5112bb1b1d337b87b8a0b37
Marcos Bolívar <mbolmun688@ieszaidinvergeles.org>
2025-10-30 09:59:53 +0100

    Ejercicio4.3

---
+++ Ejercicios4/Ejercicio3.py
@@ -0,0 +1,9 @@
+#Escribir un programa que pida al usuario un número entero positivo y muestre por
+#pantalla todos los números impares desde 1 hasta ese número separados por
+#comas.
+num = int(input("Ingrese un número: "))
+cont = 1
+while cont <= num:
+    if cont % 2 != 0:
+        print(cont)
+    cont = cont + 1
