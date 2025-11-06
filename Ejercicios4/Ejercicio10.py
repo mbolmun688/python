a31d018ed98ee0910155bd7ef40d9d7b38b1c4ac
mbolmun688 <mbolmun688@ieszaidinvergeles.org>
2025-11-06 09:28:28 +0100

    Ejercicio4.11

--- Ejercicios4/Ejercicio10.py
+++ Ejercicios4/Ejercicio10.py
@@ -1,23 +1,11 @@
 # Escribir un programa que pida al usuario un número entero y muestre por pantalla si
 # es un número primo o no.
 
-salida = 1
-
-while salida >= 1:
-    print("Pulse 0 para salir del programa")
-    num = int(input("Elija un número para saber si es primo: "))
-    if num == 0:
-        print("Saliendo...")
-        break
-    if num < 2:
-        print(f"{num} no es primo.")
-        continue
-    es_primo = True
-    for i in range(2, num):
-        if num % i == 0:
-            es_primo = False
-            break
-    if es_primo:
-        print(f"{num} es primo.")
-    else:
-        print(f"{num} no es primo.")2
+n = int(input("Introduce un número entero positivo mayor que 2: "))
+i = 2
+while n % i != 0:
+    i += 1
+if i == n:
+    print(str(n) + " es primo")
+else:
+    print(str(n) + " no es primo")
