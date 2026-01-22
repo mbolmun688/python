fa1b41b29987989564acf33a38877f62d3ffd18e
Marcos Bolívar <mbolmun688@ieszaidinvergeles.org>
2026-01-22 10:52:10 +0100

    Ejercicio6.6

---
+++ Ejercicios6-Diccionarios/Ejercicio6.py
@@ -0,0 +1,14 @@
+#Escribir un programa que cree un diccionario vacío y lo vaya llenado con
+#información sobre una persona (por ejemplo nombre, edad, sexo, teléfono, correo
+#electrónico, etc.) que se le pida al usuario. Cada vez que se añada un nuevo dato
+#debe imprimirse el contenido del diccionario.
+
+persona = {}
+while True:
+    clave = input("Ingrese el dato a registrar (o 'fin' para terminar): ")
+    if clave == "fin":
+        break
+    valor = input("Ingrese el valor: ")
+    persona[clave] = valor
+    print(persona)
+print (persona)
