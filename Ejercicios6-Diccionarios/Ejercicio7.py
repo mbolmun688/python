321446ea584f1b6c4774584cf97fa68ed603625b
mbolmun688 <mbolmun688@ieszaidinvergeles.org>
2026-01-25 18:40:01 +0100

    Ejercicio6.7

---
+++ Ejercicios6-Diccionarios/Ejercicio7.py
@@ -0,0 +1,17 @@
+#Escribir un programa que cree un diccionario simulando una cesta de la compra. El programa debe preguntar el artículo 
+#y su precio y añadir el par al diccionario, hasta que el usuario decida terminar. Después se debe mostrar por pantalla la lista 
+#de la compra y el coste total, con el siguiente formato
+
+cesta = {}
+continuar = True
+while continuar:
+    item = input('Introduce un articulo: ')
+    precio = float(input('Introduce el precio: '))
+    cesta[item] = precio
+    continuar = input('Quieres seguir (Si/No): ') == "Si"
+coste = 0
+print('Lista de la compra')
+for item, precio in cesta.items():
+    print(item, '\t', precio)
+    coste += precio
+print('Coste total: ', coste)
